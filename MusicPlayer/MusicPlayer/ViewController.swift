38f0c2606f8d74b001161baa1e5a77536747dd77
Jaemin-Mac <jaemin.ko98@gmail.com>
2022-12-11 23:22:14 +0900

    my first App

---
+++ MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 고재민 on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
